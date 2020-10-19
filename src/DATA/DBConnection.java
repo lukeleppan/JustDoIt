1cb2fabf4d441cdf19baf42e13e62ba69417d7c6
Luke Leppan <lukelepp@gmail.com>
2020-10-19 07:51:01 +0200

    Fix Issues

--- src/DATA/DBConnection.java
+++ src/DATA/DBConnection.java
@@ -1,55 +1,51 @@
 package DATA;
 
 import java.sql.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DBConnection {
 
-  private Connection conn;
-  Statement stmt;
+	private Connection conn;
+	Statement stmt;
 
-  public DBConnection() {
+	public DBConnection() {
 
-    try {
-      String dbURL = "jdbc:ucanaccess://JustDoItDatabase.accdb";
-      conn = DriverManager.getConnection(dbURL, "", "");
-      stmt = conn.createStatement();
+		try {
+			String dbURL = "jdbc:ucanaccess://JustDoItDatabase.accdb";
+			conn = DriverManager.getConnection(dbURL, "", "");
+			stmt = conn.createStatement();
 
-    } catch (SQLException ex) {
-      Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (SQLException ex) {
+			Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
 
-    }
+		}
 
-  }
+	}
 
-  public ResultSet query(String sql) throws SQLException {
-    ResultSet result = stmt.executeQuery(sql);
-    return result;
+	public ResultSet query(String sql) throws SQLException {
+		ResultSet result = stmt.executeQuery(sql);
+		return result;
 
-  }
+	}
 
-  public int update(String sql) throws SQLException {
-    Statement stmt = conn.createStatement();
-    int done = stmt.executeUpdate(sql);
-    return done;
+	public int update(String sql) throws SQLException {
+		Statement stmt = conn.createStatement();
+		int done = stmt.executeUpdate(sql);
+		return done;
 
-  }
+	}
 
-  public int updateReturnID(String sql) throws Exception {
-    Statement stmt = conn.createStatement();
-    int id = -1;
-    stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-    ResultSet result = stmt.getGeneratedKeys();
-    if (result.next()) {
-      id = result.getInt(sql);
+	public int updateReturnID(String sql) throws SQLException {
+		Statement stmt = conn.createStatement();
+		int id = -1;
+		stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+		ResultSet result = stmt.getGeneratedKeys();
+		if (result.next()) {
+			id = result.getInt(sql);
 
-    }
-    return id;
-  }
+		}
+		return id;
+	}
 
 }
--- src/MANAGER/UserManager.java
+++ src/MANAGER/UserManager.java
@@ -5,6 +5,7 @@ import DATA.User;
 import DATA.UserCreds;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,45 +13,91 @@ import java.util.logging.Logger;
 
 public class UserManager {
 
-  private DBConnection DBCon = new DBConnection();
-  private User user;
-  private UserCreds userCreds;
+	private DBConnection DBCon = new DBConnection();
+	private User user;
+	private UserCreds userCreds;
 
-  public boolean CheckForUser(String username) {
-    boolean exists = false;
+	public boolean CheckForUser(String username) {
+		boolean exists = false;
 
-    try {
-      List<UserCreds> userCredsShareableList = new ArrayList();
+		try {
+			List<UserCreds> userCredsShareableList = new ArrayList();
 
-      ResultSet rs = DBCon.query("SELECT * FROM tblUserCreds");
+			ResultSet rs = DBCon.query("SELECT * FROM tblUserCreds");
 
-      while (rs.next()) {
-        UserCreds userCred = new UserCreds(
-                rs.getInt("UserCredID"),
-                rs.getString("Username"),
-                rs.getString("Password")
-        );
-        userCredsShareableList.add(userCred);
-      }
+			while (rs.next()) {
+				UserCreds userCred = new UserCreds(
+								rs.getInt("UserCredID"),
+								rs.getString("Username"),
+								rs.getString("Password")
+				);
+				userCredsShareableList.add(userCred);
+			}
 
-      rs.close();
+			rs.close();
 
-      for (int i = 0; i < userCredsShareableList.size(); i++) {
-        if (userCredsShareableList.get(i).getUsername().equals(username)) {
-          exists = true;
-        }
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
-    }
+			for (int i = 0; i < userCredsShareableList.size(); i++) {
+				if (userCredsShareableList.get(i).getUsername().equals(username)) {
+					exists = true;
+				}
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-    return exists;
-  }
+		return exists;
+	}
 
-  public boolean RegisterUser(User user, UserCreds userCreds) {
-    boolean success = false;
+	public boolean RegisterUser(User user, UserCreds userCreds) {
+		boolean success = false;
+		int UserID = 0;
+		try {
+			UserID = DBCon.updateReturnID(
+							"INSERT INTO tblUserCreds (Username, Password)"
+							+ "VALUES ('" + userCreds.getUsername() + "', '" + userCreds.getPassword() + "');"
+			);
+		} catch (SQLException ex) {
+			Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-    return success;
-  }
+//		List<UserCreds> userCredsShareableList = new ArrayList();
+//		try {
+//			ResultSet rs = DBCon.query("SELECT * FROM tblUserCreds");
+//
+//			while (rs.next()) {
+//				UserCreds userCred = new UserCreds(
+//								rs.getInt("UserCredID"),
+//								rs.getString("Username"),
+//								rs.getString("Password")
+//				);
+//				userCredsShareableList.add(userCred);
+//			}
+//
+//			rs.close();
+//		} catch (SQLException ex) {
+//			Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+//		}
+//		for (int i = 0; i < userCredsShareableList.size(); i++) {
+//			if (userCredsShareableList.get(i).getUsername().equals(userCreds.getUsername())) {
+//				UserID = userCredsShareableList.get(i).getUserCredID();
+//			}
+//		}
+		try {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			String dateStr = simpleDateFormat.format(user.getUserDOB());
+			DBCon.update(
+							"INSERT INTO tblUsers "
+							+ "VALUES (" + UserID + ", '"
+							+ user.getUserEmail()
+							+ "', #" + dateStr + "#, '"
+							+ user.getUserFirstName() + "', '"
+							+ user.getUserSurname() + "');"
+			);
+		} catch (SQLException ex) {
+			Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return success;
+	}
 
 }
