b933120a96b4ff76257fa20e6c3a3260e1b87fad
Luke Leppan <lukelepp@gmail.com>
2020-10-23 12:40:10 +0200

    .

--- src/DATA/Projects.java
+++ src/DATA/Projects.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DATA;
 
-/**
- *
- * @author Luke Leppan
- */
 public class Projects {
+
+	private int ProjectID;
+	private int UserID;
 
 }
