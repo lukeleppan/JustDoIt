347c80dff1bcf80d28a9aa1cc2ef7528886c1834
Luke Leppan <lukelepp@gmail.com>
2021-06-16 17:28:03 +0200

    .

--- src/DATA/DBConnection.java
+++ src/DATA/DBConnection.java
@@ -6,46 +6,34 @@ import java.util.logging.Logger;
 
 public class DBConnection {
 
-	private Connection conn;
-	Statement stmt;
+    private Connection conn;
+    private Statement stmt;
 
-	public DBConnection() {
+    public DBConnection() {
 
-		try {
-			String dbURL = "jdbc:ucanaccess://JustDoItDatabase.accdb";
-			conn = DriverManager.getConnection(dbURL, "", "");
-			stmt = conn.createStatement();
+        try {
+            String dbURL = "jdbc:ucanaccess://JustDoItDatabase.accdb";
+            conn = DriverManager.getConnection(dbURL, "", "");
+            stmt = conn.createStatement();
 
-		} catch (SQLException ex) {
-			Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
 
-		}
+        }
 
-	}
+    }
 
-	public ResultSet query(String sql) throws SQLException {
-		ResultSet result = stmt.executeQuery(sql);
-		return result;
+    public ResultSet query(String sql) throws SQLException {
+        ResultSet result = stmt.executeQuery(sql);
+        return result;
 
-	}
+    }
 
-	public int update(String sql) throws SQLException {
-		Statement stmt = conn.createStatement();
-		int done = stmt.executeUpdate(sql);
-		return done;
+    public int update(String sql) throws SQLException {
+        Statement stmt = conn.createStatement();
+        int done = stmt.executeUpdate(sql);
+        return done;
 
-	}
-
-	public int updateReturnID(String sql) throws SQLException {
-		Statement stmt = conn.createStatement();
-		int id = -1;
-		stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-		ResultSet result = stmt.getGeneratedKeys();
-		if (result.next()) {
-			id = result.getInt(sql);
-		}
-		System.out.println(id);
-		return id;
-	}
+    }
 
 }
--- src/DATA/Subtask.java
+++ src/DATA/Subtask.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DATA;
 
-/**
- *
- * @author Luke Leppan
- */
 public class Subtask {
+
+    private int SubtaskID;
+    private int TaskID;
+    private String subtaskTitle;
+    private boolean subtaskCompleted;
+
+    public Subtask(int SubtaskID, int TaskID, String subtaskTitle, boolean subtaskCompleted) {
+        this.SubtaskID = SubtaskID;
+        this.TaskID = TaskID;
+        this.subtaskTitle = subtaskTitle;
+        this.subtaskCompleted = subtaskCompleted;
+    }
+
+    public int getSubtaskID() {
+        return SubtaskID;
+    }
+
+    public int getTaskID() {
+        return TaskID;
+    }
+
+    public String getSubtaskTitle() {
+        return subtaskTitle;
+    }
+
+    public boolean isSubtaskCompleted() {
+        return subtaskCompleted;
+    }
+
+    public void setSubtaskID(int SubtaskID) {
+        this.SubtaskID = SubtaskID;
+    }
+
+    public void setTaskID(int TaskID) {
+        this.TaskID = TaskID;
+    }
+
+    public void setSubtaskTitle(String subtaskTitle) {
+        this.subtaskTitle = subtaskTitle;
+    }
+
+    public void setSubtaskCompleted(boolean subtaskCompleted) {
+        this.subtaskCompleted = subtaskCompleted;
+    }
 
 }
